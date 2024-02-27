140720c75bb940c3abe5ac135424c93c6ad6b804
Damir <kdamir2004@gmail.com>
2024-02-27 20:08:43 +0600

    first commit

--- README.md
+++ README.md
@@ -1,15 +1,50 @@
-"# Go_Project" 
-#Эндпоинты
+# Go_Project
 
+
+## Medical Clinic REST API
+```
 POST /patients: Создать нового пациента.
 GET /patients/:id: Получить информацию о пациенте по его ID.
 PUT /patients/:id: Обновить информацию о пациенте по его ID.
 DELETE /patients/:id: Удалить пациента по его ID.
+
 POST /doctors: Создать нового врача.
 GET /doctors/:id: Получить информацию о враче по его ID.
 PUT /doctors/:id: Обновить информацию о враче по его ID.
 DELETE /doctors/:id: Удалить врача по его ID.
+
 POST /appointments: Создать новую запись на прием.
 GET /appointments/:id: Получить информацию о записи на прием по ее ID.
 PUT /appointments/:id: Обновить информацию о записи на прием по ее ID.
 DELETE /appointments/:id: Удалить запись на прием по ее ID.
+```
+
+## DB Structure
+```
+Table patients {
+  id bigserial [primary key]
+  created_at timestamp
+  updated_at timestamp
+  name text
+  birthdate date
+  gender text
+}
+
+Table doctors {
+  id bigserial [primary key]
+  created_at timestamp
+  updated_at timestamp
+  name text
+  specialty text
+}
+
+// many-to-many
+Table appointments {
+  id bigserial [primary key]
+  created_at timestamp
+  updated_at timestamp
+  patient_id bigint [ref: > patients.id]
+  doctor_id bigint [ref: > doctors.id]
+  date_time DATETIME
+}
+```
---
+++ pkg/clinic-api/migrations/000001_create_medicalclinics_table.up.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS patients 
+(
+  id            bigserial PRIMARY KEY,
+  created_at    timestamp(0) with time zone NOT NULL DEFAULT now(),
+  updated_at    timestamp(0) with time zone NOT NULL DEFAULT now(),
+  name          text                        NOT NULL,
+  birthdate     date                        NOT NULL,
+  gender        text                        NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS doctors
+(
+    id            bigserial PRIMARY KEY,
+    created_at    timestamp(0) with time zone NOT NULL DEFAULT now(),
+    updated_at    timestamp(0) with time zone NOT NULL DEFAULT now(),
+    name          text                        NOT NULL,
+    specialty     text                        NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS appointments
+(
+    id            bigserial PRIMARY KEY,
+    created_at    timestamp(0) with time zone NOT NULL DEFAULT now(),
+    updated_at    timestamp(0) with time zone NOT NULL DEFAULT now(),
+    patient_id    bigint                      NOT NULL,
+    doctor_id     bigint                      NOT NULL,
+    date          date                        NOT NULL,
+    start_time    time                        NOT NULL,
+    end_time      time                        NOT NULL,
+    status        text                        NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients (id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors (id)
+);
